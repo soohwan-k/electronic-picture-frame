a27001b0a58ff33c3459499377b31cdbfdf53b15
SooHwan <newpoo@naver.com>
2022-12-05 21:16:24 +0900

    feat: 권한 부여시 갤러리 열기

--- app/src/main/java/org/tech/town/electronicpictureframe/MainActivity.kt
+++ app/src/main/java/org/tech/town/electronicpictureframe/MainActivity.kt
@@ -1,9 +1,12 @@
 package org.tech.town.electronicpictureframe
 
+import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 
@@ -15,6 +18,18 @@ class MainActivity : AppCompatActivity() {
 
     private val startPhotoFrameModeButton: Button by lazy {
         findViewById<Button>(androidx.coordinatorlayout.R.id.start)
+    }
+
+    private val imageViewList: List<ImageView> by lazy {
+        mutableListOf<ImageView>().apply {
+            add(findViewById(R.id.imageView11))
+            add(findViewById(R.id.imageView12))
+            add(findViewById(R.id.imageView13))
+            add(findViewById(R.id.imageView21))
+            add(findViewById(R.id.imageView22))
+            add(findViewById(R.id.imageView23))
+        }
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,6 +48,7 @@ class MainActivity : AppCompatActivity() {
                     android.Manifest.permission.READ_EXTERNAL_STORAGE,
                 ) == PackageManager.PERMISSION_GRANTED -> {
                     //TODO 권한이 잘 부여되었을 때 갤러리에서 사진을 선택하는 기능
+                    navigatePhotos()
                 }
                 shouldShowRequestPermissionRationale(android.Manifest.permission.READ_EXTERNAL_STORAGE) -> {
                     //교육용 팝업 확인 후 권한 팝업을 띄우는 기능
@@ -48,6 +64,40 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    //권한 요청 결과에 대한 콜백
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            1000 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //권한이 부여 되었을 때
+                    navigatePhotos()
+                }
+                else{
+                    Toast.makeText(this, "권한을 거부하셨습니다.", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+            else -> {
+                //1000 이외의 다른 requestCode 요청한 적 없기 때문에 구현x
+            }
+        }
+
+    }
+
+    //SAF
+    private fun navigatePhotos() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
+        startActivityForResult(intent, 2000)
+    }
+
 
     private fun showPermissionContextPopup() {
         AlertDialog.Builder(this)
